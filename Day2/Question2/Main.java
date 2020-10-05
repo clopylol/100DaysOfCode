7b30d1998d3d266ad290175dc4d31fa983330fc3
clopylol <berkay.ertugrul@hotmail.com>
2020-10-05 20:16:13 +0300

    Day 3 Finished v1

--- Day2/Question2/Main.java
+++ Day2/Question2/Main.java
@@ -1,56 +1,50 @@
 import java.util.Scanner;
-public class Main
-{
-	public static void main(String[] args) {
+public class Main {
+    public static void main(String[] args) {
         System.out.print("--- Temperature Conversion ---");
         Scanner sc = new Scanner(System.in);
         System.out.print("Pls Enter the Temperature: ");
         double temperature = sc.nextInt();
-    
+
         System.out.println("Select Your Choice \n > Celsius to Fahrenheit = 0 \n > Fahrenheit to Celsius = 1 ");
         double choice = sc.nextInt();
 
-    
-        if(choice == 0){
+
+        if (choice == 0) {
             System.out.println("Transaction Successful ! \nTemperature is: " + celciusToFahrenheit(temperature) + " Fahrenheit");
 
-        }
-        else if(choice == 1){
+        } else if (choice == 1) {
             System.out.println("Transaction Successful ! \nTemperature is: " + fahrenheitToCelcius(temperature) + " Celsius");
-        
-        }
-        else{
+
+        } else {
             System.out.println("Error: Pls Select 0 or 1 !");
         }
- 
-    }
-	
-	
-	/*
-	
-	This function using for Celsius to Fahrenheit
 
-    Formule = Celsius * 1.8 + 32 
-    
+    }
+
+
+    /*
+	    This function using for Celsius to Fahrenheit
+        Formula = Celsius * 1.8 + 32 
     */
 
-    public static double celciusToFahrenheit(double temperature){
+    public static double celciusToFahrenheit(double temperature) {
         double celToFah;
         celToFah = ((temperature * 1.8) + 32);
         return celToFah;
     }
-    
-    
-    /* This function using for Fahrenheit to Celsius
-    
-    Formule = (Fahrenheit - 32) / 1.8 
-    
+
+
+    /* 
+        This function using for Fahrenheit to Celsius
+        Formula = (Fahrenheit - 32) / 1.8 
     */
-    public static double fahrenheitToCelcius(double temperature){
+    
+    public static double fahrenheitToCelcius(double temperature) {
         double fahToTem;
         fahToTem = ((temperature - 32) / 1.8);
         return fahToTem;
     }
-	
-	
+
+
 }
--- Day2/Question3/Main.java
+++ Day2/Question3/Main.java
@@ -1,73 +1,57 @@
-
 import java.util.Random;
 public class Main
-
 {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-		
-		//Fonksiyonumuz bize zarToplamlarini döndürecek.
-		int zarToplamlari;
-		int yeniZarToplamlari;
+    public static void main(String[] args) {
+        System.out.println("---CRAPS GAME---");
+        int zarToplamlari;
+        int yeniZarToplamlari;
+
         zarToplamlari = zarAt();
-        
-	    //System.out.println("Zarların Toplamı: " + zarToplamlari + "\n");
-		
-		if (zarToplamlari == 7 || zarToplamlari == 11){
-		    System.out.println("1.Tur:" + zarToplamlari + " geldi. Oyuncu Kazandı !");
-		    System.out.println("Akıyo bu akşam, Maşallah");
-		    //break;
-		}
-		else if (zarToplamlari == 2 || zarToplamlari == 3 || zarToplamlari == 12){
-		    System.out.println("1.Tur: " + zarToplamlari + " geldi. Oyuncu Kaybetti !");
-		    System.out.println("Bari zardan düşseydik Ekrem Abi");
-		    //break;
-		}
-		//4-5-6-8-9-10 gelme durumu.
-		else{
-		    System.out.println("1.Tur: " + zarToplamlari + " geldi. \nSonuç Belli Değil. 7 gelmeden yine " + zarToplamlari + " atarsa kazanacak !\n");
-		    int gelecekZar = zarToplamlari;
-		    for (int i = 2; i < 100 ; i++){
-		    
-		        
-		            yeniZarToplamlari = zarAt();
-		            
-		            if(yeniZarToplamlari == 7){
-		                System.out.println(i +".Tur: Zar Toplamları 7 Geldi ! OYUN BİTTİ. \n----------LOSE-------------");
-		                break;
-		            }
-	            
-		            if(yeniZarToplamlari == gelecekZar){
-		                System.out.println(i +".Tur: " + yeniZarToplamlari + " geldi. OYUNCU KAZANDI ! \n+++++++++ WİN +++++++++");
-		                break;
-		            }
-		            
-		            if(yeniZarToplamlari != gelecekZar || yeniZarToplamlari != 7){
-		                System.out.println(i +".Tur: " + yeniZarToplamlari + " geldi. Sonuç Belli Değil.  Oyuncu Yeniden Zar Atacak!\n");
-		            }
-		        } 
-		        
-		    }
 
-	}
-	
-	public static int zarAt(){
-		int min = 1;
-		int max = 7;
-		int zarToplam = 0;
-		Random r = new Random();
-		
-		for (int i = 1; i <= 2; i++){
-		    
-		    //int zarNum = r.nextInt((max - min) + 1)) + min;
-		    
-		    int zarNum = (int)(Math.random()*6) + 1;
-		    System.out.println(i + ". Zar " + zarNum + " geldi.");
-		    zarToplam += zarNum;
-		}
-		return zarToplam;
-	}
-	
+        if (zarToplamlari == 7 || zarToplamlari == 11) {
+            System.out.println("1.Tur:" + zarToplamlari + " geldi. Oyuncu Kazandı !");
+            System.out.println("Akıyo bu akşam, Maşallah");
+        } 
+		else if (zarToplamlari == 2 || zarToplamlari == 3 || zarToplamlari == 12) {
+            System.out.println("1.Tur: " + zarToplamlari + " geldi. Oyuncu Kaybetti !");
+            System.out.println("Bari zardan düşseydik Ekrem Abi");
+        }
+
+        //4-5-6-8-9-10 gelme durumu.
+        else {
+            System.out.println("1.Tur: " + zarToplamlari + " geldi. \nSonuç Belli Değil. 7 gelmeden yine " + zarToplamlari + " atarsa kazanacak !\n");
+            int gelecekZar = zarToplamlari;
+            for (int i = 2; i < 100; i++) {
+
+                yeniZarToplamlari = zarAt();
+
+                if (yeniZarToplamlari == 7) {
+                    System.out.println(i + ".Tur: Zar Toplamları 7 Geldi ! OYUN BİTTİ. \n----------LOSE-------------");
+                    break;
+                }
+                if (yeniZarToplamlari == gelecekZar) {
+                    System.out.println(i + ".Tur: " + yeniZarToplamlari + " geldi. OYUNCU KAZANDI ! \n++++++++++WIN++++++++++");
+                    break;
+                }
+                if (yeniZarToplamlari != gelecekZar || yeniZarToplamlari != 7) {
+                    System.out.println(i + ".Tur: " + yeniZarToplamlari + " geldi. Sonuç Belli Değil.  Oyuncu Yeniden Zar Atacak!\n");
+                }
+            }
+        }
+    }
+
+    public static int zarAt() {
+        int min = 1;
+        int max = 7;
+        int zarToplam = 0;
+        Random r = new Random();
+
+        for (int i = 1; i <= 2; i++) {
+            int zarNum = (int)(Math.random() * 6) + 1;
+            System.out.println(i + ". Zar " + zarNum + " geldi.");
+            zarToplam += zarNum;
+        }
+        return zarToplam;
+    }
+
 }
-
-
