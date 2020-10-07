3f3e25d4df57464bf7e829f2b751f694aa7757e5
clopylol <berkay.ertugrul@hotmail.com>
2020-10-07 21:56:20 +0300

    README Update

--- Day5/Question1/Main.java
+++ Day5/Question1/Main.java
@@ -2,18 +2,16 @@ import java.util.Scanner;
 
 public class Main
 {
-	public static void main(String[] args) {
-	    
+	public static void main(String[] args) {   
 	    boolean state;
-	    int number;
-	    
+	    int number;    
 		System.out.println("---is a Prime---");
+
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Please enter number: ");
-		number = sc.nextInt();
-		
+		number = sc.nextInt();	
 	    state = isPrime(number);
-	    
+		
 	    if(state){
 	        System.out.print(number + " is a Prime !");
 	    }
